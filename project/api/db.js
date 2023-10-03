e4b30bf51693a286852ff99d22bc743593a3f3a2
rashmidaswaththa <rashmidaswaththa@gmail.com>
2023-10-03 13:23:53 +0530

    Modify .env file

--- project/api/db.js
+++ project/api/db.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 module.exports = () => {
+	const user = process.env.MONGO_USER;
+	const password = process.env.MONGO_PASSWORD;
+	const cluster = process.env.MONGO_CLUSTER;
+	const database = process.env.MONGO_DATABASE;
+
+	const uri = `mongodb+srv://${user}:${password}@${cluster}/${database}?retryWrites=true&w=majority`;
+
+
 	const connection = mongoose
-		.connect(process.env.MONGODB_URI)
+		.connect(uri)
 		.then((result) => console.log("Connected to database"))
 		.catch((err) => console.log("could not connect to database"));
 };
